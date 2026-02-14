08b641f142d96e51d468dfcbbe7122d70f0cc9d1
陳宛伶 <erin20080306gmail.com@chenwanlingdeMacBook-Air.local>
2026-02-14 09:31:53 +0800

    功能：PWA 自動更新，偵測到新版本時自動刷新頁面

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
+
+// PWA 自動更新：偵測到新版本時自動刷新頁面
+const updateSW = registerSW({
+  onNeedRefresh() {
+    // 有新版本時自動更新
+    updateSW(true)
+  },
+  onOfflineReady() {
+    console.log('PWA 離線模式已準備就緒')
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
